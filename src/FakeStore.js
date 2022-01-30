041f591e7279ee9b7e3d8747f6362199f01f36a8
jluisgomezmontal <montalgex18@gmail.com>
2022-01-30 15:59:15 -0600

    checkout agregado

--- src/FakeStore.js
+++ src/FakeStore.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const FakeStore = () => {
   return (
 <div className="header">
-          <h1><Link to='/'>MijoTest</Link></h1>
+          <h1><Link to='/'>FakeStore</Link></h1>
           <div className="navegacion">            
               <Link to='/categorias'>Categorias</Link>
               <Link to='/carrito'>Carrito</Link>
